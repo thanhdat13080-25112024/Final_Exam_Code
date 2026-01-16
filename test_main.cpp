2866595252a3b647b92539510e36dcd750bf1351
thanhdat <thanhdat13080@gmail.com>
2026-01-16 20:29:11 +0700

    find func

--- test_main.cpp
+++ test_main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 class candidate_attribute {
     private:
-        int id;
+        string id;
         string name;
         string room;
         double score;
@@ -24,7 +24,7 @@ class candidate_attribute {
                 return true;
             }
         }
-        void input_func() {
+        void input() {
             cout<<"Enter the id of student: "<<endl;
             cin>>id;
             while (isValidName(name)) {
@@ -45,6 +45,26 @@ class candidate_attribute {
                     break;
                 } 
                 }
+        }
+        void display(int index) {
+            cout<<"Information of candidate "<<index+1<<endl;
+            cout<<"ID: "<<id<<endl;
+            cout<<"Name: "<<name<<endl;
+            cout<<"Room: "<<room<<endl;
+            cout<<"Score: "<<score<<endl;
+        }
+        void find(string search_id) {
+            cout<<"Enter the id to search: "<<endl;
+            cin>>search_id;
+            for (char c : id) {
+                for (char d : search_id) {
+                    if (c == d) {
+                        cout<<"Found candidate with ID: "<<search_id<<endl;
+                    }
+                }
+                
+            }
+
         }
 };
 int main() {

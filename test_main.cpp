e6d5da03d21a7c23779b1451a194d8ea79514cf6
thanhdat <thanhdat13080@gmail.com>
2026-01-16 21:57:29 +0700

    add more chuc nang

--- test_main.cpp
+++ test_main.cpp
@@ -15,14 +15,16 @@ class candidate_attribute {
                 return false;
             } 
             for (char c : name) {
-                while (isdigit(c)) {
+                if (isdigit(c)) {
                     return false;
-                }
-                if(c == '!' || c == '@' || c == '$' || c == '%' || c == '&' || c == '*') {
+                } else if(c == '!' & c == '@' & c == '$' & c == '%' & c == '&' & c == '*') {
                     return false;
+                        } else {
+                            continue;
                 }
                 return true;
             }
+            return true;
         }
         void input_func() {
             cout<<"Enter the id of student: "<<endl;
@@ -41,12 +43,62 @@ class candidate_attribute {
             while (true) {
                 cout<<"Enter the score: "<<endl;
                 cin>>score;
-                if (score <= 0 || score >= 10) {
+                if (score >= 0 & score <= 10) {
                     break;
+                } else {
+                    continue;
                 } 
                 }
         }
+        void max(double score) {
+            //dang phat trien
+        }
+        void min(double score) {
+
+        }
 };
 int main() {
-    
+    list<candidate_attribute> candidates;
+    int choice;
+    while (true) {
+    cout<<"------Menu Candidate management ------"<<endl;
+    cout<<"1. Add more candidates"<<endl;
+    cout<<"2. Display the list of candidates."<<endl;
+    cout<<"3. Search for candidates"<<endl;
+    cout<<"4. Remove candidate"<<endl;
+    cout<<"5. Sort candidates by score"<<endl;
+    cout<<"6. Exit"<<endl;  
+    cin>>choice;
+    if (choice >= 6 & choice <= 0) {
+        cout<<"Try again!";
+        continue;
+    } else {
+        break;
+    }
+    }
+    switch (choice) {
+        case 1: {
+            //dang phat trien
+            break;
+        }
+        case 2: {
+            //dang phat trien
+            break;
+        }
+        case 3: {
+            //dang phat trien
+            break;
+        }
+        case 4: {
+            //dang phat trien
+            break;
+        }
+        case 5: {
+            //dang phat trien
+            break;
+        }
+        case 6: {
+            break;
+        }
+    }
 }
